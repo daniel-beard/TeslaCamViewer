3ae4c245f88edb691e2ddc16eabb6a7fb864c0c5
dbeard <dbeard@groupon.com>
2019-05-05 11:27:14 -0700

    Initial Commit

---
+++ TeslaCamViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TeslaCamViewer
+//
+//  Created by Daniel Beard on 5/5/19.
+//  Copyright © 2019 dbeard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TeslaCamViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TeslaCamViewer
+//
+//  Created by Daniel Beard on 5/5/19.
+//  Copyright © 2019 dbeard. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
