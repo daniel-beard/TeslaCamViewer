59f7c5aebde08bbb4610864f6d05dd47d48f02aa
dbeard <dbeard@groupon.com>
2019-05-11 09:18:23 -0700

    Time observers, progress bar, play next video support

--- TeslaCamViewer/ViewController.swift
+++ TeslaCamViewer/ViewController.swift
@@ -21,10 +21,21 @@ class ViewController: NSViewController {
     var rightAVPlayer: AVPlayer?
     var avPlayers: [AVPlayer]?
 
+    var leftTimeObserver: Any?
+    var centerTimeObserver: Any?
+    var rightTimeObserver: Any?
+
+    var progressDict = [AVPlayer: Double]()
+    var leftProgress: CMTime?
+    var centerProgress: CMTime?
+    var rightProgress: CMTime?
+
+    var videoKeysRemaining: [Date]?
+    var videos: DirectoryCrawler?
+
     //MARK: Reactive properties
     var isPlaying: Bool = false {
         didSet {
-            print("isPlaying changed to: \(isPlaying)")
             self.playButton.title = isPlaying ? "⏸" : "▶️"
         }
     }
@@ -34,9 +45,11 @@ class ViewController: NSViewController {
         }
     }
 
+
     //MARK: UI
     @IBOutlet weak var playButton: NSButton!
     @IBOutlet weak var speedSegmentControl: NSSegmentedControl!
+    @IBOutlet weak var progressBar: NSProgressIndicator!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,18 +59,78 @@ class ViewController: NSViewController {
         centerPlayerView.controlsStyle = .none
         rightPlayerView.controlsStyle = .none
 
-        let videos = DirectoryCrawler()
+        self.videos = DirectoryCrawler()
 
         //TODO: No crash operator
-        let firstVideo = videos.first!.value
-        setVideoPlayers(to: firstVideo, playAutomatically: false)
+        // for testing
+        let prefixedVideoKeys = videos!.videoDictionary.keys.sorted().dropFirst(8)
 
+        let earliestDate = prefixedVideoKeys.last!
+        self.videoKeysRemaining = prefixedVideoKeys.dropLast()
+        let firstVideo = videos!.videoDictionary[earliestDate]!
+        setVideoPlayers(to: firstVideo, playAutomatically: false)
+    }
+
+    // Adds a time observer for updating the progress bar
+    func timeObserver(for player: AVPlayer) -> Any {
+        // Invoke callback every half second
+        let interval = CMTime(seconds: 0.5,
+                              preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+        // Queue on which to invoke the callback
+        let mainQueue = DispatchQueue.main
+        // Add time observer
+        let timeObserverToken =
+            player.addPeriodicTimeObserver(forInterval: interval, queue: mainQueue) {
+                [weak self] time in
+
+                if let duration = player.currentItem?.duration {
+                    self?.progressDict[player] = player.currentTime().seconds / duration.seconds * 100
+                } else {
+                    self?.progressDict[player] = 0.0
+                }
+                self?.refreshProgressBar()
+                self?.pollCheckForEndOfVideo()
+        }
+        return timeObserverToken
+    }
+
+    func refreshProgressBar() {
+        // Take the largest of all, since some videos may not load in triplets.
+        self.progressBar.doubleValue = self.progressDict.values.max() ?? 0
+    }
+
+    // Calculates if any of the three videos have ended, and if so,
+    // move on to the next video in our list, if we've got one
+    func pollCheckForEndOfVideo() {
+        let currentProgress = self.progressDict.values.max() ?? 0
+        if currentProgress >= 100 {
+
+            // Play next video, if we have one
+            if (self.videoKeysRemaining?.count ?? 0) > 0 {
+                print("Boom")
+                let earliestNextDate = self.videoKeysRemaining!.last!
+                self.videoKeysRemaining = self.videoKeysRemaining?.dropLast()
+                let firstVideo = videos!.videoDictionary[earliestNextDate]!
+
+                self.progressDict = [AVPlayer: Double]()
+
+                setVideoPlayers(to: firstVideo, playAutomatically: true)
+            } else {
+                //TODO: here's where we should tear down the polling timers.
+            }
+
+        }
     }
 
     func setVideoPlayers(to video: [TeslaCamVideo], playAutomatically: Bool) {
         let leftVideo = video.first(where: { $0.cameraType == .left })
         let centerVideo = video.first(where: { $0.cameraType == .front })
         let rightVideo = video.first(where: { $0.cameraType == .right })
+
+        // remove any previous time observers if we had 'em
+        leftAVPlayer?.removeTimeObserver(leftTimeObserver as Any)
+        centerAVPlayer?.removeTimeObserver(centerTimeObserver as Any)
+        rightAVPlayer?.removeTimeObserver(rightTimeObserver as Any)
 
         // Load the videos into AVPlayers
         leftAVPlayer = AVPlayer(url: leftVideo?.fileURL ?? URL(fileURLWithPath: ""))
@@ -70,22 +143,17 @@ class ViewController: NSViewController {
         self.rightPlayerView.player = rightAVPlayer
         avPlayers = [leftAVPlayer!, centerAVPlayer!, rightAVPlayer!]
 
+        // setup new time observers
+        leftTimeObserver = timeObserver(for: leftAVPlayer!)
+        centerTimeObserver = timeObserver(for: centerAVPlayer!)
+        rightTimeObserver = timeObserver(for: rightAVPlayer!)
+
         guard playAutomatically == true else { return }
-        self.leftAVPlayer?.play()
-        self.centerAVPlayer?.play()
-        self.rightAVPlayer?.play()
+        avPlayers?.forEach({ [weak self] (avPlayer: AVPlayer) in
+            avPlayer.play()
+            avPlayer.rate = self?.playbackRate ?? 0
+        })
     }
-
-
-    @IBAction func playbackSpeedControlTapped(_ sender: NSSegmentedControl) {
-        switch sender.selectedSegment {
-            case 0: playbackRate = 1
-            case 1: playbackRate = 2
-            case 2: playbackRate = 5
-            default: break
-        }
-    }
-
 
 }
 
@@ -102,6 +170,15 @@ extension ViewController {
         isPlaying = !isPlaying
     }
 
+    @IBAction func playbackSpeedControlTapped(_ sender: NSSegmentedControl) {
+        switch sender.selectedSegment {
+        case 0: playbackRate = 1
+        case 1: playbackRate = 2
+        case 2: playbackRate = 5
+        case 3: playbackRate = 10
+        default: break
+        }
+    }
 
 }
 
